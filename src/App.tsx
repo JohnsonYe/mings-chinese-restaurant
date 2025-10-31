d70ca5d9a9ae9b72d7e102bc61b02ecfa1640533
JohnsonYe <johnsonye0303@gmail.com>
2025-10-30 20:18:51 -0700

    Fix GitHub Pages routing with basename and 404 redirect

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/mings-chinese-restaurant">
       <div className="app">
         <Navigation />
         <main className="main-content">
